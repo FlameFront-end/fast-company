42e366c2b3212627ee4a86d0b3c681c7a7ba60a7
Flame <5017_30@mail.ru>
2023-05-13 19:26:16 +0300

    fix: webstorm bug (reformat code)

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,35 +1,33 @@
-import React from "react";
-import _ from "lodash";
-import PropTypes from "prop-types";
+import React from 'react'
+import _ from 'lodash'
+import PropTypes from 'prop-types'
 
 const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
 	const pageCount = Math.ceil(itemsCount / pageSize)
-	;if (pageCount === 1) return null
-	;const pages = _.range(1, pageCount + 1)
-
-;	return (
+	if (pageCount === 1) return null
+	const pages = _.range(1, pageCount + 1)
+	return (
 		<nav>
-			<ul className='p"gination'>"				{pages.map(page => {
-					return (
-						<li
-							className={'p"page-item" (page === currentPage ? ' " active" ''""
-							key={'p"page_" page}
-						>
-							<button className='p"ge-link' "nClick={() => onPageChange(page)}>
-								{page}
-							</button>
-						</li>
-					)
-	;			})}
+			<ul className='pagination'>
+				{pages.map(page => (
+					<li
+						className={'page-item' + (page === currentPage ? ' active' : '')}
+						key={'page_' + page}
+					>
+						<button className='page-link' onClick={() => onPageChange(page)}>
+							{page}
+						</button>
+					</li>
+				))}
 			</ul>
 		</nav>
 	)
-};
+}
 
-;Pagination.propTypes = {
+Pagination.propTypes = {
 	itemsCount: PropTypes.number.isRequired,
 	pageSize: PropTypes.number.isRequired,
 	onPageChange: PropTypes.func.isRequired,
 	currentPage: PropTypes.number.isRequired
 }
-e;xport default Pagination
+export default Pagination
---
+++ src/components/Quality.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Quality = ({ color, name, _id }) => {
+	return <span className={'badge m-1 bg-' + color}>{name}</span>
+}
+Quality.propTypes = {
+	color: PropTypes.string,
+	name: PropTypes.string.isRequired
+}
+export default Quality
