ab4e8a5ac74d8f607e666b6191ec96d874f6f5dd
Flame <5017_30@mail.ru>
2023-05-18 01:14:41 +0300

    feat: add login form

---
+++ src/utils/validator.js
@@ -0,0 +1,46 @@
+export function validator(data, config) {
+	const errors = {}
+	function validate(validatedMethod, data, config) {
+		let statusValidate
+		switch (validatedMethod) {
+			case 'isRequired':
+				statusValidate = data.trim() === ''
+				break
+			case 'isEmail': {
+				const emailRegExp = /^\S+@\S+\.\S+$/g
+				statusValidate = !emailRegExp.test(data)
+				break
+			}
+			case 'isCapitalSymbol': {
+				const capitalRegExp = /[A-Z]+/g
+				statusValidate = !capitalRegExp.test(data)
+				break
+			}
+			case 'isContainDigit': {
+				const digitRegExp = /\d+/g
+				statusValidate = !digitRegExp.test(data)
+				break
+			}
+			case 'min': {
+				statusValidate = data.length < config.value
+				break
+			}
+			default:
+				break
+		}
+		if (statusValidate) return config.message
+	}
+	for (const fieldName in data) {
+		for (const validatedMethod in config[fieldName]) {
+			const error = validate(
+				validatedMethod,
+				data[fieldName],
+				config[fieldName][validatedMethod]
+			)
+			if (error && !errors[fieldName]) {
+				errors[fieldName] = error
+			}
+		}
+	}
+	return errors
+}
