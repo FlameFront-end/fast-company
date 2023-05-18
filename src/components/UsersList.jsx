9db637a8bd1a860b30d2f1a071364ba01d0f0f22
Flame <5017_30@mail.ru>
2023-05-18 16:24:08 +0300

    feat: add search users

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -12,6 +12,7 @@ const UsersList = () => {
 	const [currentPage, setCurrentPage] = useState(1)
 	const [professions, setProfession] = useState()
 	const [selectedProf, setSelectedProf] = useState()
+	const [searchQuery, setSearchQuery] = useState('')
 	const [sortBy, setSortBy] = useState({ path: 'name', order: 'asc' })
 	const pageSize = 8
 
@@ -39,9 +40,10 @@ const UsersList = () => {
 
 	useEffect(() => {
 		setCurrentPage(1)
-	}, [selectedProf])
+	}, [selectedProf, searchQuery])
 
 	const handleProfessionSelect = item => {
+		if (searchQuery !== '') setSearchQuery('')
 		setSelectedProf(item)
 	}
 
@@ -51,9 +53,18 @@ const UsersList = () => {
 	const handleSort = item => {
 		setSortBy(item)
 	}
+	const handleSearchQuery = ({ target }) => {
+		setSelectedProf(undefined)
+		setSearchQuery(target.value)
+	}
 
 	if (users) {
-		const filteredUsers = selectedProf
+		const filteredUsers = searchQuery
+			? users.filter(
+					user =>
+						user.name.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1
+			  )
+			: selectedProf
 			? users.filter(
 					user =>
 						JSON.stringify(user.profession) === JSON.stringify(selectedProf)
@@ -84,6 +95,13 @@ const UsersList = () => {
 				)}
 				<div className='d-flex flex-column'>
 					<SearchStatus length={count} />
+					<input
+						type='text'
+						name='searchQuery'
+						placeholder='Поиск...'
+						onChange={handleSearchQuery}
+						value={searchQuery}
+					/>
 					{count > 0 && (
 						<UsersTable
 							users={usersCrop}
