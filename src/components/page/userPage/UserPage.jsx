10380b5186140a60d60c7ad65c4b604858ab8f4d
Flame <5017_30@mail.ru>
2023-05-19 15:20:54 +0300

    feat: add loader

--- src/components/page/userPage/UserPage.jsx
+++ src/components/page/userPage/UserPage.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import api from '../../../api'
 import QualitiesList from '../../ui/qualities/QualitiesList'
 import { useNavigate } from 'react-router-dom'
+import style from '../loader.module.css'
 
 const UserPage = ({ userId }) => {
 	const navigate = useNavigate()
@@ -28,7 +29,53 @@ const UserPage = ({ userId }) => {
 				</button>
 			</div>
 		)
-	} else return <h1>Loading</h1>
+	} else
+		return (
+			<div className={style.loader_wrapper}>
+				<svg
+					className={style.pl}
+					viewBox='0 0 200 200'
+					width='200'
+					height='200'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<defs>
+						<linearGradient id='pl-grad1' x1='1' y1='0.5' x2='0' y2='0.5'>
+							<stop offset='0%' stop-color='hsl(313,90%,55%)' />
+							<stop offset='100%' stop-color='hsl(223,90%,55%)' />
+						</linearGradient>
+						<linearGradient id='pl-grad2' x1='0' y1='0' x2='0' y2='1'>
+							<stop offset='0%' stop-color='hsl(313,90%,55%)' />
+							<stop offset='100%' stop-color='hsl(223,90%,55%)' />
+						</linearGradient>
+					</defs>
+					<circle
+						className={style.pl__ring}
+						cx='100'
+						cy='100'
+						r='82'
+						fill='none'
+						stroke='url(#pl-grad1)'
+						stroke-width='36'
+						stroke-dasharray='0 257 1 257'
+						stroke-dashoffset='0.01'
+						stroke-linecap='round'
+						transform='rotate(-90,100,100)'
+					/>
+					<line
+						className={style.pl__ball}
+						stroke='url(#pl-grad2)'
+						x1='100'
+						y1='18'
+						x2='100.01'
+						y2='182'
+						stroke-width='36'
+						stroke-dasharray='1 165'
+						stroke-linecap='round'
+					/>
+				</svg>
+			</div>
+		)
 }
 
 UserPage.propTypes = {
--- src/components/page/usersListPage/UsersListPage.jsx
+++ src/components/page/usersListPage/UsersListPage.jsx
@@ -7,6 +7,7 @@ import GroupList from '../../common/GroupList'
 import SearchStatus from '../../ui/SearchStatus'
 import UsersTable from '../../ui/UsersTable'
 import _ from 'lodash'
+import style from '../loader.module.css'
 
 const UsersListPage = () => {
 	const [currentPage, setCurrentPage] = useState(1)
@@ -124,7 +125,52 @@ const UsersListPage = () => {
 			</div>
 		)
 	}
-	return 'loading...'
+	return (
+		<div className={style.loader_wrapper}>
+			<svg
+				className={style.pl}
+				viewBox='0 0 200 200'
+				width='200'
+				height='200'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<defs>
+					<linearGradient id='pl-grad1' x1='1' y1='0.5' x2='0' y2='0.5'>
+						<stop offset='0%' stop-color='hsl(313,90%,55%)' />
+						<stop offset='100%' stop-color='hsl(223,90%,55%)' />
+					</linearGradient>
+					<linearGradient id='pl-grad2' x1='0' y1='0' x2='0' y2='1'>
+						<stop offset='0%' stop-color='hsl(313,90%,55%)' />
+						<stop offset='100%' stop-color='hsl(223,90%,55%)' />
+					</linearGradient>
+				</defs>
+				<circle
+					className={style.pl__ring}
+					cx='100'
+					cy='100'
+					r='82'
+					fill='none'
+					stroke='url(#pl-grad1)'
+					stroke-width='36'
+					stroke-dasharray='0 257 1 257'
+					stroke-dashoffset='0.01'
+					stroke-linecap='round'
+					transform='rotate(-90,100,100)'
+				/>
+				<line
+					className={style.pl__ball}
+					stroke='url(#pl-grad2)'
+					x1='100'
+					y1='18'
+					x2='100.01'
+					y2='182'
+					stroke-width='36'
+					stroke-dasharray='1 165'
+					stroke-linecap='round'
+				/>
+			</svg>
+		</div>
+	)
 }
 UsersListPage.propTypes = {
 	users: PropTypes.array
